d3f224a364c5c42419ec96fb0bac05cc8234a824
Pim de Bruin <pedebruin@hotmail.com>
2021-04-02 12:17:30 +0200

    Figure changes

--- Flextures/flextureAnalysis.m
+++ Flextures/flextureAnalysis.m
@@ -1,3 +1,4 @@
+
 close all; clear;
 set(0,'defaultTextInterpreter','latex');
 
@@ -6,14 +7,14 @@ set(0,'defaultTextInterpreter','latex');
 %}
 
 %% Parameters
-    E = 160*10^9;        % E modulus [Pa]
+    E = 190*10^9;       % E modulus [Pa]
     t = 25e-6;          % height of the layer [m]
-    w = 6.5e-6;           % width of the bending beams [m]
-    L = 800e-6;         % length of the bending beams [m]
+    w = 6.5e-6;         % width of the bending beams [m]
+    L = 1150e-6;        % length of the bending beams [m]
     d = 40e-6;          % Spacing of the beams [m]
-    s_req = 22e-6;      % Required stroke (from center) [m]
+    s_req = 50e-6;      % Required stroke (from center) [m]
     
-    n = 4;              % number of folded flextures in parallel (now one per side)
+    n = 2;              % number of folded flextures in parallel (now one per side)
     
 %% Calculation
 % Stifness
@@ -34,39 +35,30 @@ set(0,'defaultTextInterpreter','latex');
     %}
     maxLeft = 2*d;          % Max deformation assuming left will touch
     maxRight = 4/3*d;       % Max deformation assuming right will touch
-    
     maxStroke = min(maxLeft, maxRight);
-
+    
 % Required force for stroke:
     F_req = k_total*s_req;
 
- 
 %% Stage kinematics
 Alpha = 30;                   % degrees
-s = 7.8868e-4;                % meters
+s = 0.782669e-3;                % meters
 beamLength = 0.815e-3;
 
-
-% kinM = [-1 0 s; 
-%         -1/sind(45) 1/cosd(45) s;
-%         1/sin(45) 1/cos(45) s];
-%        
-% posCombs = [0 0 0]';    
-% posxyt = kinM\posCombs;
-% 
-
-
-x = -22.26e-6;
-y = 23.45e-6;
-theta = 9.657e-2;
+x = 50e-6;  %6.059738292e-06;
+y = 75e-6; %-7.088136361e-09;
+theta = 15;
 
 R = [cosd(theta), -sind(theta);
     sind(theta), cosd(theta)];
 
-A = [0, s]';
+A = [0, 1]'*s;
+B = [-cosd(30), sind(30)]'*s;
 C = [-cosd(30), -sind(30)]'*s;
+D = [0, -1]'*s;
 E = [cosd(30), -sind(30)]'*s;
-stagePos0 = [A, C, E];
+F = [cosd(30), sind(30)]'*s;
+stagePos0 = [A, B, C, D, E, F];
 
 % ankers
 a = A + beamLength*[1, 0]';
@@ -81,8 +73,8 @@ eL0 = E-e;
 % deform
 stagePos = R*stagePos0+[x, y]'; 
 A1 = stagePos(:,1);
-C1 = stagePos(:,2);
-E1 = stagePos(:,3);
+C1 = stagePos(:,3);
+E1 = stagePos(:,5);
 
 % new lengths
 aL1 = A1-a;
@@ -94,41 +86,82 @@ Aactuation = norm(aL1)-norm(aL0);
 Cactuation = norm(cL1)-norm(cL0);
 Eactuation = norm(eL1)-norm(eL0);
 
+% Forces
+AF = 2*(k_total * Aactuation + 3.6279e-05*theta);
+CF = 2*(k_total * Cactuation + 3.6279e-05*theta);
+EF = 2*(k_total * Eactuation + 3.6279e-05*theta);
 
+p = cell(11,1);
+%% Plot!
 figure('Name','STAGESSSSS')
 hold on
+title('Kinematic stage analysis')
+xlabel('X position [m]')
+ylabel('Y position [m]')
 ax = gca;
+
 % Fixed points
-plot(ax,a(1),a(2),'ok'); 
-plot(ax,c(1),c(2),'ok');
-plot(ax,e(1),e(2),'ok');
+p{1} = plot(ax,a(1),a(2),'ok'); 
+p{2} = plot(ax,c(1),c(2),'ok');
+p{3} = plot(ax,e(1),e(2),'ok');
+
 % Original
-patch(ax, stagePos0(1,:),stagePos0(2,:),'r');       % Original
-plot(ax, [A(1), a(1)], [A(2), a(2)],'r');
-plot(ax, [C(1), c(1)], [C(2), c(2)],'r');
-plot(ax, [E(1), e(1)], [E(2), e(2)],'r');
+p{4} = patch(ax, stagePos0(1,:),stagePos0(2,:),'k');       % Original
+p{5} = plot(ax, [A(1), a(1)], [A(2), a(2)],'k');
+p{6} = plot(ax, [C(1), c(1)], [C(2), c(2)],'k');
+p{7} = plot(ax, [E(1), e(1)], [E(2), e(2)],'k');
 
-% % Displaced
-% patch(ax, stagePos(1,:),stagePos(2,:),'c');         % Displaced
-% plot(ax, [A1(1), a(1)], [A1(2), a(2)],'c');
-% plot(ax, [C1(1), c(1)], [C1(2), c(2)],'c');
-% plot(ax, [E1(1), e(1)], [E1(2), e(2)],'c');
+% Displaced
+p{8} = patch(ax, stagePos(1,:),stagePos(2,:),'k');         % Displaced
+p{9} = plot(ax, [A1(1), a(1)], [A1(2), a(2)],'k');
+p{10} = plot(ax, [C1(1), c(1)], [C1(2), c(2)],'k');
+p{11} = plot(ax, [E1(1), e(1)], [E1(2), e(2)],'k');
 
+pv = zeros(length(p),1);
+for i = 1:length(p)
+    pv(i) = p{i};
+end
 
+% Important points
+p{12} = text(ax,a(1)+2e-5,a(2),'$$a$$','FontSize',18);
+p{13} = text(ax,c(1)+2e-5,c(2),'$$c$$','FontSize',18);
+p{14} = text(ax,e(1)+2e-5,e(2),'$$e$$','FontSize',20);
+
+legend(pv([4,8]),'Original location','Displaced location');
 axis equal
-alpha(0.3)
+grid on
 
+% Original
+alpha(pv(4:7),0.65)          % Patch surface
+set(pv(4),'EdgeAlpha',0.4); % Patch lines
+p{5}.Color(4) = 0.75;      % plotted lines
+p{6}.Color(4) = 0.75;
+p{7}.Color(4) = 0.75;
+
+% Displaced
+alpha(pv(8:11),0.35)
+set(pv(8),'EdgeAlpha',0.1);
+p{9}.Color(4) = 0.35;
+p{10}.Color(4) = 0.35;
+p{11}.Color(4) = 0.35;
   
 %% Results
 % Stifnessessss
 fprintf(['------------- Results -------------\n',...
-         'k_total = %4.2f [N/m] \n',...
-         'max stroke = %4.2f [um] \n',...
-         'F required = %4.2f [N] \n'], k_total, maxStroke*10^6, F_req)
-
+         'k_total = %4.9f [N/m] \n',...
+         'max stroke = %4.9f [um] \n',...
+         'F required = %4.9f [N] \n'], k_total, maxStroke*10^6, F_req)
 % Displacements    
-fprintf(['-> A stroke %4.9f [m] \n',...
-            '-> C stroke %4.9f [m] \n',...
-            '-> E stroke %4.9f [m] \n'],Aactuation, Cactuation, Eactuation);
+fprintf([   '-> A stroke: %4.9f  [m] \n',...
+            '      Force:  %4.9f [N] \n',...
+            '-> C stroke: %4.9f  [m] \n',...
+            '      Force:  %4.9f [N] \n',...
+            '-> E stroke: %4.9f  [m] \n'...
+            '      Force:  %4.9f [N] \n'],Aactuation, AF, Cactuation, CF, Eactuation, EF);
+fprintf([ '->POS: \n',...
+          '    x = %4.9f [m] \n',...
+          '    y = %4.9f [m] \n',...
+          'theta = %4.9f [deg] \n'],x,y,theta);
+    
     
     
